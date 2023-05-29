67735a58f78cb166331d98a277781a9350ac226a
Eros Baixauli <eros.baixauli@innovait.cat>
2023-05-29 11:39:04 +0200

    Terminados ejercicios hasta el 12

--- Ejercicios/01-Introduccion/scripts/app.js
+++ Ejercicios/01-Introduccion/scripts/app.js
@@ -31,3 +31,41 @@ let entries = Object.entries(obj);
 console.log("Keys:", keys);
 console.log("Values:", values);
 console.log("Entries:", entries);
+
+//Using forEach and map
+const exampleArray = ["platanos","naranjas","manzanas","peras","fresas"];
+
+exampleArray.forEach((fruta,posicion)=>{
+    console.log(posicion+": "+fruta);         //ForEach browse every element and executes the code inside the function
+});
+
+const fruits = exampleArray.map((fruta)=>{
+    return `I have ${fruta}`;         //map does the same, but creating a new array with the result
+});
+console.log(fruits);
+
+//Ejercicio Fizz buzz
+for (let x=1;x<=100;x++){
+    if (x%3===0 && x%5===0){
+        console.log(`FIZZBUZZ!! (${x})`);
+    }else if(x%3===0){
+        console.log(`Fizz (${x})`);
+    }else if(x%5===0){
+        console.log(`Buzz!! (${x})`);
+    }
+}
+
+const arr = [1, 2, 3, 4, 5];
+for (let [index, value] of arr.entries()) {
+  console.log(typeof index, value);
+}
+
+const arr2 = [1, 2, 3, 4, 5];
+for (let [index, value] of Object.entries(arr2)) {
+  console.log(typeof index, value);
+}
+
+
+const array = [1,2,3,4,5];
+let resultado = array.reduce((total, values)=> total + values,0);
+console.log ("resultado: "+resultado);
